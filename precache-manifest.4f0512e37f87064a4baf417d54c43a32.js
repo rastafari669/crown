839bcafa8323ae91bf50e8eb27f2f4d530f4658c
hugendubell <joeykraus789@gmail.com>
2021-03-09 15:17:55 +0200

    Updates

---
+++ precache-manifest.4f0512e37f87064a4baf417d54c43a32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1d5da1879757cf5b807c492d5d4aa8",
+    "url": "/crown/index.html"
+  },
+  {
+    "revision": "869d1738b57e465df2a0",
+    "url": "/crown/static/css/main.d10eac5d.chunk.css"
+  },
+  {
+    "revision": "3aae2894b13f963e068c",
+    "url": "/crown/static/js/2.5d2a7ad3.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/crown/static/js/2.5d2a7ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869d1738b57e465df2a0",
+    "url": "/crown/static/js/main.3c2463ec.chunk.js"
+  },
+  {
+    "revision": "162b7935e58308294911",
+    "url": "/crown/static/js/runtime-main.54654592.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crown/static/media/shopping-bag.30f07071.svg"
+  }
+]);
